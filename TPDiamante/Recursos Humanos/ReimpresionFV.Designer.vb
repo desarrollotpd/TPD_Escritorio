c68e7d10852b59cd53c183dbf342e0157a7c71c7
Jorge Luis Garcia <licjorgeluis@hotmail.com>
2023-02-09 12:08:35 -0600

    Modulo de RH

--- TPDiamante/Recursos Humanos/ReimpresionFV.Designer.vb
+++ TPDiamante/Recursos Humanos/ReimpresionFV.Designer.vb
@@ -1,134 +1,135 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class ReimpresionFV
-    Inherits System.Windows.Forms.Form
+ Inherits System.Windows.Forms.Form
 
-    'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
-    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-        Try
-            If disposing AndAlso components IsNot Nothing Then
-                components.Dispose()
-            End If
-        Finally
-            MyBase.Dispose(disposing)
-        End Try
-    End Sub
+ 'Form reemplaza a Dispose para limpiar la lista de componentes.
+ <System.Diagnostics.DebuggerNonUserCode()>
+ Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+  Try
+   If disposing AndAlso components IsNot Nothing Then
+    components.Dispose()
+   End If
+  Finally
+   MyBase.Dispose(disposing)
+  End Try
+ End Sub
 
-    'Requerido por el Diseñador de Windows Forms
-    Private components As System.ComponentModel.IContainer
+ 'Requerido por el Diseñador de Windows Forms
+ Private components As System.ComponentModel.IContainer
 
-    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
-    'Se puede modificar usando el Diseñador de Windows Forms.  
-    'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
-    Private Sub InitializeComponent()
-        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
-        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.CBNomEmp = New System.Windows.Forms.ComboBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SplitContainer1.Panel1.SuspendLayout()
-        Me.SplitContainer1.Panel2.SuspendLayout()
-        Me.SplitContainer1.SuspendLayout()
-        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'SplitContainer1
-        '
-        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
-        Me.SplitContainer1.Name = "SplitContainer1"
-        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
-        '
-        'SplitContainer1.Panel1
-        '
-        Me.SplitContainer1.Panel1.Controls.Add(Me.DataGridView2)
-        Me.SplitContainer1.Panel1.Controls.Add(Me.Label1)
-        Me.SplitContainer1.Panel1.Controls.Add(Me.CBNomEmp)
-        Me.SplitContainer1.Panel1.Controls.Add(Me.Label2)
-        '
-        'SplitContainer1.Panel2
-        '
-        Me.SplitContainer1.Panel2.Controls.Add(Me.DataGridView1)
-        Me.SplitContainer1.Size = New System.Drawing.Size(620, 391)
-        Me.SplitContainer1.SplitterDistance = 77
-        Me.SplitContainer1.TabIndex = 0
-        '
-        'DataGridView2
-        '
-        Me.DataGridView2.AllowUserToAddRows = False
-        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView2.Location = New System.Drawing.Point(21, 22)
-        Me.DataGridView2.Name = "DataGridView2"
-        Me.DataGridView2.Size = New System.Drawing.Size(61, 12)
-        Me.DataGridView2.TabIndex = 624
-        Me.DataGridView2.Visible = False
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(100, 9)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(466, 24)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "REIMPRESION DE FORMATO DE VACACIONES"
-        '
-        'CBNomEmp
-        '
-        Me.CBNomEmp.FormattingEnabled = True
-        Me.CBNomEmp.Location = New System.Drawing.Point(117, 50)
-        Me.CBNomEmp.Name = "CBNomEmp"
-        Me.CBNomEmp.Size = New System.Drawing.Size(193, 21)
-        Me.CBNomEmp.TabIndex = 605
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(14, 52)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(97, 13)
-        Me.Label2.TabIndex = 604
-        Me.Label2.Text = "Nombre y Apellidos"
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.AllowUserToAddRows = False
-        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(620, 310)
-        Me.DataGridView1.TabIndex = 1
-        '
-        'ReimpresionFV
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(620, 391)
-        Me.Controls.Add(Me.SplitContainer1)
-        Me.Name = "ReimpresionFV"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "ReimpresionFV"
-        Me.SplitContainer1.Panel1.ResumeLayout(False)
-        Me.SplitContainer1.Panel1.PerformLayout()
-        Me.SplitContainer1.Panel2.ResumeLayout(False)
-        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.SplitContainer1.ResumeLayout(False)
-        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
 
-    End Sub
+ 'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+ 'Se puede modificar usando el Diseñador de Windows Forms.  
+ 'No lo modifique con el editor de código.
+ <System.Diagnostics.DebuggerStepThrough()>
+ Private Sub InitializeComponent()
+  Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+  Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+  Me.Label1 = New System.Windows.Forms.Label()
+  Me.CBNomEmp = New System.Windows.Forms.ComboBox()
+  Me.Label2 = New System.Windows.Forms.Label()
+  Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+  CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+  Me.SplitContainer1.Panel1.SuspendLayout()
+  Me.SplitContainer1.Panel2.SuspendLayout()
+  Me.SplitContainer1.SuspendLayout()
+  CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+  CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+  Me.SuspendLayout()
+  '
+  'SplitContainer1
+  '
+  Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+  Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+  Me.SplitContainer1.Name = "SplitContainer1"
+  Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+  '
+  'SplitContainer1.Panel1
+  '
+  Me.SplitContainer1.Panel1.Controls.Add(Me.DataGridView2)
+  Me.SplitContainer1.Panel1.Controls.Add(Me.Label1)
+  Me.SplitContainer1.Panel1.Controls.Add(Me.CBNomEmp)
+  Me.SplitContainer1.Panel1.Controls.Add(Me.Label2)
+  '
+  'SplitContainer1.Panel2
+  '
+  Me.SplitContainer1.Panel2.Controls.Add(Me.DataGridView1)
+  Me.SplitContainer1.Size = New System.Drawing.Size(620, 391)
+  Me.SplitContainer1.SplitterDistance = 77
+  Me.SplitContainer1.TabIndex = 0
+  '
+  'DataGridView2
+  '
+  Me.DataGridView2.AllowUserToAddRows = False
+  Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+  Me.DataGridView2.Location = New System.Drawing.Point(21, 22)
+  Me.DataGridView2.Name = "DataGridView2"
+  Me.DataGridView2.Size = New System.Drawing.Size(61, 12)
+  Me.DataGridView2.TabIndex = 624
+  Me.DataGridView2.Visible = False
+  '
+  'Label1
+  '
+  Me.Label1.AutoSize = True
+  Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+  Me.Label1.Location = New System.Drawing.Point(100, 9)
+  Me.Label1.Name = "Label1"
+  Me.Label1.Size = New System.Drawing.Size(466, 24)
+  Me.Label1.TabIndex = 0
+  Me.Label1.Text = "REIMPRESION DE FORMATO DE VACACIONES"
+  '
+  'CBNomEmp
+  '
+  Me.CBNomEmp.FormattingEnabled = True
+  Me.CBNomEmp.Location = New System.Drawing.Point(117, 50)
+  Me.CBNomEmp.Name = "CBNomEmp"
+  Me.CBNomEmp.Size = New System.Drawing.Size(193, 21)
+  Me.CBNomEmp.TabIndex = 605
+  '
+  'Label2
+  '
+  Me.Label2.AutoSize = True
+  Me.Label2.Location = New System.Drawing.Point(14, 52)
+  Me.Label2.Name = "Label2"
+  Me.Label2.Size = New System.Drawing.Size(97, 13)
+  Me.Label2.TabIndex = 604
+  Me.Label2.Text = "Nombre y Apellidos"
+  '
+  'DataGridView1
+  '
+  Me.DataGridView1.AllowUserToAddRows = False
+  Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+  Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+  Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+  Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
+  Me.DataGridView1.Name = "DataGridView1"
+  Me.DataGridView1.Size = New System.Drawing.Size(620, 310)
+  Me.DataGridView1.TabIndex = 1
+  '
+  'ReimpresionFV
+  '
+  Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+  Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+  Me.ClientSize = New System.Drawing.Size(620, 391)
+  Me.Controls.Add(Me.SplitContainer1)
+  Me.Name = "ReimpresionFV"
+  Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+  Me.Text = "ReimpresionFV"
+  Me.SplitContainer1.Panel1.ResumeLayout(False)
+  Me.SplitContainer1.Panel1.PerformLayout()
+  Me.SplitContainer1.Panel2.ResumeLayout(False)
+  CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+  Me.SplitContainer1.ResumeLayout(False)
+  CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+  CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+  Me.ResumeLayout(False)
 
-    Friend WithEvents SplitContainer1 As SplitContainer
-    Friend WithEvents Label1 As Label
-    Friend WithEvents DataGridView2 As DataGridView
-    Friend WithEvents CBNomEmp As ComboBox
-    Friend WithEvents Label2 As Label
-    Friend WithEvents DataGridView1 As DataGridView
+ End Sub
+
+ Friend WithEvents SplitContainer1 As SplitContainer
+ Friend WithEvents Label1 As Label
+ Friend WithEvents DataGridView2 As DataGridView
+ Friend WithEvents CBNomEmp As ComboBox
+ Friend WithEvents Label2 As Label
+ Friend WithEvents DataGridView1 As DataGridView
 End Class
